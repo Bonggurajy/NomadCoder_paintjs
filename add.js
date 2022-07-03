2fedcf36c331b2f41545e27e5a13e6d8494d39a6
Bonggurajy <ajinsuk3@naver.com>
2022-07-04 08:56:12 +0900

    #2.1 2D Context

--- add.js
+++ add.js
@@ -1,18 +1,33 @@
 const canvas = document.querySelector('#jsCanvas')
+const ctx = canvas.getContext('2d');
+
+ctx.strokeStyle = 'black';
+ctx.lineWidth = 1;
+
+canvas.width = 550;
+canvas.height = 600;
 
 let painting = false;
 //MouseEvent Functions
+function startPainting () {
+	painting = true;
+}
+
 function onMouseMove (event) {
 	const x = event.offsetX;
 	const y = event.offsetY;
+	if(!painting) {
+		ctx.beginPath();
+		ctx.moveTo(x,y);
+	}
+	else {
+		ctx.lineTo(x,y);
+		ctx.stroke();
+	}
 }
 
 function onMouseDown (event) {
 	painting = true;
-}
-
-function onMouseUp(event) {
-	stopPainting();
 }
 
 //Functions
@@ -22,7 +37,8 @@ function stopPainting () {
 
 if(canvas) {
 	canvas.addEventListener('mousemove', onMouseMove)
-	canvas.addEventListener('mousedown', onMouseDown)
-	canvas.addEventListener('mouseup', onMouseUp)
+	canvas.addEventListener('mousedown', startPainting)
+	canvas.addEventListener('mouseup', stopPainting)
 	canvas.addEventListener('mouseleave', stopPainting)
 }
+
